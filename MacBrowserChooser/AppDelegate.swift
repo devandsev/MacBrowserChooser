b56780ea4b87ae53b418e8ae53af7af77aa045a5
Andrey Sevrikov <devandsev@gmail.com>
2017-12-23 16:23:09 +0300

    Initial Commit

---
+++ MacBrowserChooser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacBrowserChooser
+//
+//  Created by Andrey Sevrikov on 23/12/2017.
+//  Copyright © 2017 devandsev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacBrowserChooser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacBrowserChooser
+//
+//  Created by Andrey Sevrikov on 23/12/2017.
+//  Copyright © 2017 devandsev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
