303d4fe3ee2572376bada337d64c6877cc6d0e51
Andrey Sevrikov <devandsev@gmail.com>
2017-12-23 18:53:35 +0300

    Add SystemBrowserService helper funcs

--- MacBrowserChooser/Source/Services/SystemBrowserService.swift
+++ MacBrowserChooser/Source/Services/SystemBrowserService.swift
@@ -17,4 +17,30 @@ class SystemBrowserService {
         
         LSSetDefaultHandlerForURLScheme("http" as CFString, bundleIdentifier as CFString)
     }
+    
+    func bundleIdentifierForDefault() -> String? {
+        return LSCopyDefaultHandlerForURLScheme("http" as CFString)?.takeRetainedValue() as? String
+    }
+    
+    func bundleIdentifiers() -> [String] {
+        guard let ids = LSCopyAllHandlersForURLScheme("http" as CFString)?.takeRetainedValue() as? [String] else {
+            return []
+        }
+        
+        return ids
+    }
+    
+    func browsersPaths() -> [URL] {
+        let ids = bundleIdentifiers()
+        
+        return ids.flatMap { self.paths(for: $0).first }
+    }
+    
+    func paths(for bundleIdentifier: String) -> [URL] {
+        guard let paths = LSCopyApplicationURLsForBundleIdentifier(bundleIdentifier as CFString, nil)?.takeRetainedValue() as? [URL] else {
+            return []
+        }
+        
+        return paths
+    }
 }
